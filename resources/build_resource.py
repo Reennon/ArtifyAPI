8fbc58ea8deaa3529e90a0b10c4126fc2e1238d5
Roman Kovalchuk <37474734+Reennon@users.noreply.github.com>
2020-12-14 00:40:48 +0200

    Arranged Core Post Endoint

--- resources/build_resource.py
+++ resources/build_resource.py
@@ -24,17 +24,46 @@ class BuildResource(Resource):
 
     def post(self):
         """
-        send Core command build project
+        Point:
+            send Core command build project
+
+        Args:
+            JSON string
+
+        JSON STRUCTURE
+             KEY                TYPE        DESC
+        {
+            "command": "build"  #string     build command tells core which command to execute
+            , "userId":         #int        User ID is required for core to return the build, user gave
+            , "dllName":        #string     Dll Name is required for core to name the build, user specified
+            , "ASSEMBLY_NAME":  #string     Assembly Name is not required for core, but preferred to have one,
+                                #           otherwise core will name it by itself
+            , "UNSAFE_CODE":    #boolean    Unsafe Code
+            , "NECESSARY_DLLS": #string[]   Necessary Dlls is not required for core, if empty, default libs will
+                                #           be linked in memory and end-product dll. Below listed default libs,
+                                #           that are required for compiled program to be executed
+                                #           "System.Private.CoreLib", "System.Console", "System.Runtime", "mscorlib"
+        }
 
         Returns:
             Http response 200
+
         """
         data = request.get_json()
-        dllName = data['dllName']
+        user_id = data["userId"]
+        dll_name = data.get("dllName", "application")
+        assembly_name = data.get("ASSEMBLY_NAME", None)
+        unsafe_code = data.get("UNSAFE_CODE", False)
+        necessary_dlls = data.get("NECESSARY_DLLS", None)
 
-        SocketConnection.socket_send(str(
+        import json
+        SocketConnection.socket_send(json.dumps(
             {
-                "command": "build",
-                "dllName": "dllName" if dllName is None else dllName
+                "command": "build"
+                , "userId": user_id
+                , "dllName": dll_name
+                , "ASSEMBLY_NAME": assembly_name
+                , "UNSAFE_CODE": unsafe_code
+                , "NECESSARY_DLLS": necessary_dlls
             }))
         return HTTPStatus.OK
