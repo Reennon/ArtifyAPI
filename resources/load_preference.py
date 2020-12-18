aedd1facf586bbaa53e45145b791ab9ca4695766
stepan9773 <stepan9773@users.noreply.github.com>
2020-12-18 03:15:10 +0200

    load preferences connect to db

--- resources/load_preference.py
+++ resources/load_preference.py
@@ -6,9 +6,14 @@ from flask import request, flash
 from flask_login import current_user
 from flask_restful import Resource
 
+from app import db
 from constants import Constants
 from models.curent_preference import Curent_user_preference
+from models.module import Module
+from models.preference_script import Preference_script
 from models.preference_user import Preference_user
+from models.preferene_module import Preference_module
+from models.script import Script
 from utils.utils import Utils
 
 
@@ -54,51 +59,44 @@ class LoadPreferenceResource(Resource):
         if os.path.exists(name_path):
             shutil.rmtree(name_path)
         print("Create")
-
         file.save(path_f)
-
         Utils.unzip_folder(path_f, name=name_path)
-        shutil.copytree(name_path, Constants.cloud_preference_folder_path(current_user), dirs_exist_ok=True)
-        print("delete")
+        files = shutil.copytree(name_path, Constants.cloud_preference_folder_path(current_user), dirs_exist_ok=True)
+        print(files)
+        script_path = Constants.cloud_script_folder_path(current_user, curent_preference)
+        module_path = Constants.cloud_module_folder_path(current_user, curent_preference)
+        modules = [f for f in os.listdir(module_path)]
+        scrptes = [f for f in os.listdir(script_path)]
+        print(scrptes)
+        for script_name in scrptes:
+            print(script_path + script_name)
+            if Script.query.filter_by(file_name=script_path + script_name).first() is None:
+                script = Script(
+                    file_name=str(Constants.cloud_script_folder_path(current_user, curent_preference) + script_name))
+                db.session.add(script)
+                db.session.commit()
+                preference_scripte = Preference_script(script_id=script.id,
+                                                       preference_id=curent_preference.preference_id)
+                db.session.add(preference_scripte)
+                print(f"NEW SCRIPT {script_name}")
+                db.session.commit()
+        for module_name in modules:
+            print(module_path + module_name)
+            if Module.query.filter_by(file_name=module_path + module_name).first() is None:
+                module = Module(
+                    file_name=str(Constants.cloud_module_folder_path(current_user, curent_preference) + module_name))
+                db.session.add(module)
+                db.session.commit()
+                preference_module = Preference_module(module_id=module.id,
+                                                      preference_id=curent_preference.preference_id)
+                db.session.add(preference_module)
+                print(f"NEW MODULE {module_name}")
+                db.session.commit()
         if os.path.exists(path_f):
             os.remove(path_f)
         if os.path.exists(name_path):
             shutil.rmtree(os.path.join("Buffer\\Preference_user_" + str(current_user.id), name))
+        print("delete")
 
-        """
-
-
-        user_preference_user = Preference_user.query.filter_by(user_id=current_user.id).first()
-        if user_preference_user is None:
-            return "please log in "
-        curent_preference = Curent_user_preference.query.filter_by(preference_user_id=user_preference_user.id,
-                                                             current_user_preference=True).first()
-        user_preference = Preference.query.filter_by(id=curent_preference.preference_id).first()
-        if not os.path.exists("Bufer"):
-            os.mkdir("Bufer")
-
-        if not os.path.exists("Bufer\\Preference_user_" + str(current_user.id)):
-            os.mkdir("Bufer\\Preference_user_" + str(current_user.id))
-        if not os.path.isdir("Bufer\\Preference_user_" + str(current_user.id) + "\\" + str(curent_preference.name)):
-            os.mkdir("Bufer\\Preference_user_" + str(current_user.id) + "\\" + str(curent_preference.name))
-            os.mkdir("Bufer\\Preference_user_" + str(current_user.id) + "\\" + str(curent_preference.name) + "\\Modules")
-            os.mkdir("Bufer\\Preference_user_" + str(current_user.id) + "\\" + str(curent_preference.name) + "\\Scripts")
-        if not os.path.exists("Preference"):
-            os.mkdir("Preference")
-            os.mkdir("Preference\\Modules")
-            os.mkdir("Preference\\Scripts")
-        file.save(os.path.join(Constants.cloud_script_folder_path(current_user, curent_preference), file.filename))
-        shutil.copy(Constants.cloud_script_folder_path(current_user, curent_preference) + file.filename,
-                    Constants.SCRIPT_FOLDER_PATH)
-
-        if Script.query.filter_by(file_name=str(
-                Constants.cloud_script_folder_path(current_user, curent_preference) + file.filename)).first() is None:
-            script = Script(
-                file_name=str(Constants.cloud_script_folder_path(current_user, curent_preference) + file.filename))
-            db.session.add(script)
-            db.session.commit()
-            preference_module = Preference_script(script_id=script.id, preference_id=curent_preference.preference_id)
-            db.session.add(preference_module)
-            db.session.commit()"""
 
         return HTTPStatus.OK
