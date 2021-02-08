8431ab7fc1530c17db5fd0127c3d8f982bef5799
stepan9773 <stepan9773@users.noreply.github.com>
2021-02-08 13:48:47 +0200

    generate file tree json

--- resources/get_folder_tree_resource.py
+++ resources/get_folder_tree_resource.py
@@ -1,22 +1,11 @@
-import json
-import os
-import shutil
-from http import HTTPStatus
-
-from flask import request, flash, make_response, jsonify, render_template
 from flask_login import current_user
 from flask_restful import Resource
 
-from app import db
-from constants import Constants
 from models.curent_preference import Curent_user_preference
-from models.preference_user import Preference_user
-from models.preference_file import Preference_file
 from models.file import File
-from utils.files import Files
-from utils.utils import Utils
-from utils.html_generate import GenerateHTML
-import json
+from models.preference_file import Preference_file
+from models.preference_user import Preference_user
+
 
 class GetFolderTreeResource(Resource):
     def get(self):
@@ -29,18 +18,19 @@ class GetFolderTreeResource(Resource):
                 }
             }
         user_preference_user = Preference_user.query.filter_by(user_id=current_user.id).first()
-        curent_preference = Curent_user_preference.query.filter_by(preference_user_id=user_preference_user.id,current_user_preference=True).first()
-        user_preference_files = Preference_file.query.filter_by(preference_id = curent_preference.preference_id).all()
-        paths=[]
+        curent_preference = Curent_user_preference.query.filter_by(preference_user_id=user_preference_user.id,
+                                                                   current_user_preference=True).first()
+        user_preference_files = Preference_file.query.filter_by(preference_id=curent_preference.preference_id).all()
+        paths = []
         for file in user_preference_files:
             paths.append((File.query.filter_by(id=file.file_id).first()).file_name)
         tree = {}
         for item in paths:
             currTree = tree
-            item=item.split('\\')
+            item = item.split('\\')
             for key in item[::]:
                 if key not in currTree:
                     currTree[key] = {}
                 currTree = currTree[key]
-        html_code = GenerateHTML.generate_file_window_tree(tree)
-        return html_code
+
+        return tree
